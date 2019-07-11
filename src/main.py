f6b416170cbb5b0e6bc517d9962d5647f2b49e1a
Sbastien Dominguez <sebdominguez@me.com>
2019-07-12 00:07:12 +0100

    sorting files

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ if has_gpio:
 
 FILES = []
 for root, dirs, files in os.walk(os.path.abspath(AUDIO_PATH)):
-    for file in files:
+    for file in sorted(files):
         if file.endswith(".mp3"):
             logger.debug('Found matching file: {}'.format(file))
             FILES.append((os.path.join(root, file),
